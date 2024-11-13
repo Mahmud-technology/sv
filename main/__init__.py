55fb4da891f401e03d6075c4c8b4cfe1336210d4
Mahmud-technology <127020512+Mahmud-technology@users.noreply.github.com>
2024-11-13 07:38:39 +0600

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = 24068031
 API_HASH = "620e80fbc06cff9e5c9a4912d8e61ba0"
 BOT_TOKEN = "7656706291:AAEk9X0CITRU6rRDsA8-o1_gvrAv4869xPs"
-SESSION = config("SESSION", default=None)
+SESSION = "BQFvP78AT-pJ-vIhN2UcfYejQRjOEBxVVKIHFHqzuq-kq7OM6HNNcc1NgGbYTBpyeEthPEe9eqOyy97KPPPD00jd9ZzIYahVFM3QnGYvyu8EaFmJPeLFmOwZsW-f1vSa-aVFzpS6ovbnaN25Wfgnjr74u5U5E2qwvLR6poiioqu3FA_POXWaMoSDb-0ubrwgn5NHd2SdLFyoh-0yVPFaCExLJmIdQBQXmOBU7CPBHoV7Z4SR8OnNuB869wzBiqoQ9Z0dlMo2O7avJl6k2SxZvm1XENDNFLThoLAF9KqLZ-xQBL8xv9wtEdpLBsFUaSX3IqdPaiqa_4iLtv3oiSshnNQ4H7TI9AAAAAFrL8EnAA"
 FORCESUB = "tech_tipsbd"
 AUTH = "6091906014"
 
